55da28f144e0175e847dda95b28e40049c88052f
Nittyanand Sable <nittyanand85@gmail.com>
2026-03-31 13:41:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function showTasks() {
+    let list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        let li = document.createElement("li");
+        li.textContent = task;
+
+        li.onclick = () => deleteTask(index);
+
+        list.appendChild(li);
+    });
+}
+
+function addTask() {
+    let input = document.getElementById("taskInput");
+    if (input.value === "") return;
+
+    tasks.push(input.value);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    input.value = "";
+    showTasks();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    showTasks();
+}
+
+showTasks();
